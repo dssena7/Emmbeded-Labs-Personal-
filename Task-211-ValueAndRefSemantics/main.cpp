504750bd688c56c759cb11960c08b9f6f4f30883
dssena7 <78347354+dssena7@users.noreply.github.com>
2023-10-13 16:05:13 +0100

    Value and Reference semantics

--- Task-211-ValueAndRefSemantics/main.cpp
+++ Task-211-ValueAndRefSemantics/main.cpp
@@ -11,21 +11,21 @@ int main() {
 
     //Create instance of a complex number
     ComplexNumber_C p = {2.0, 3.0};
-    printf("p = %f + j%f\n", p.real, p.imag);
+    printf(" (1)p = %f + j%f\n", p.real, p.imag);
     
     //Create another 
     ComplexNumber_C q = {0.0, 0.0};
-    printf("q = %f + j%f\n", q.real, q.imag);
+    printf("(2)q = %f + j%f\n", q.real, q.imag);
 
     // 1 - Assign one to another
     q = p;
-    printf("q = %f + j%f\n", q.real, q.imag);
+    printf("(3)q = %f + j%f\n", q.real, q.imag);  // Values = 2 + j3
 
-    // 2 - Change p. Is q affected?
+    // 2 - Change p. Is q affected?  Answer = no
     p.real = -1.0;
     p.imag = -2.0;
-    printf("p = %f + j%f\n", p.real, p.imag);
-    printf("q = %f + j%f\n", q.real, q.imag);
+    printf("(4)p = %f + j%f\n", p.real, p.imag);
+    printf("(5)q = %f + j%f\n", q.real, q.imag);
 
     // 3 - Now create a REFERENCE TYPE (C pointer type)
     ComplexNumber_C* ptrP;   //Note the type is a pointer (32-bit address)
@@ -34,15 +34,15 @@ int main() {
     ptrP->real = 200.0;
 
     // Are either p and q affected? Why?
-    printf("p = %f + j%f\n", p.real, p.imag);
-    printf("q = %f + j%f\n", q.real, q.imag);    
+    printf("(6)p = %f + j%f\n", p.real, p.imag);
+    printf("(7)q = %f + j%f\n", q.real, q.imag);    
 
     // 4 - Another reference type (C++ reference type)
     ComplexNumber_C& refQ = q;
     refQ.real = 33.0;
     refQ.imag = 66.0;
-    printf("p = %f + j%f\n", p.real, p.imag);
-    printf("q = %f + j%f\n", q.real, q.imag);   
+    printf("(8)p = %f + j%f\n", p.real, p.imag);
+    printf("(9)q = %f + j%f\n", q.real, q.imag);   
 
     // 5 - Converting a C pointer to a C++ reference
     ComplexNumber_C& nice_ptr = *ptrP;
